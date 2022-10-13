9f9c5da9d135dbd5c0460e3e18286bd71619e594
Allen <allenim@appstate.edu>
2022-10-13 13:42:27 -0400

    modified animations

--- lib/widgets/TurbineGauge.dart
+++ lib/widgets/TurbineGauge.dart
@@ -6,41 +6,47 @@ import 'package:syncfusion_flutter_gauges/gauges.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class TurbineGauge extends StatefulWidget {
+  const TurbineGauge({Key? key}) : super(key: key);
   @override _TurbineGaugeState createState() => _TurbineGaugeState();
 }
 
-class _TurbineGaugeState extends State<TurbineGauge> {
-
-  double _finPos = 0;
+class _TurbineGaugeState extends State<TurbineGauge> with TickerProviderStateMixin  {
 
   late Timer _timer;
+  int _speed = 1;
+
+  late final AnimationController _controller = AnimationController(
+    duration: Duration(seconds: _speed),
+    vsync: this,
+  )..repeat(reverse: false);
+
+  // Create an animation with value of type "double"
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.linear,
+  );
 
   final Stream _dB = FirebaseFirestore.instance.collection('kioskData')
       .orderBy('time', descending: true)
       .limit(1)
       .snapshots(includeMetadataChanges: true);
 
-  void _updateFinPosition() {
-    if (mounted) {
-      setState(() {
-        _finPos = (_finPos + 1) % 360;
-      });
-    }
-  }
-
-  void _setSpeed(Timer t1) {
-    _timer = t1;
-    _updateFinPosition();
-  }
-
   void _setWindDBSpeed(val) {
     if (mounted) {
       setState(() {
         double s = double.parse(val);
-        int speed = 100 - s.toInt();
-        if (speed < 20) speed = 20;
-        Timer.periodic(Duration(milliseconds: speed), (timer) { _setSpeed(timer);});
-      });
+        _speed = s.toInt();
+        _speed -= 100;
+        if (_speed < 10) {
+          _speed = 10;
+        } else if (_speed > 100) {
+          _speed = 100;
+        }
+          _controller.duration = Duration(seconds: _speed);
+          _controller.repeat();
+        });
+
+      print(_speed);
     }
   }
 
@@ -56,137 +62,118 @@ class _TurbineGaugeState extends State<TurbineGauge> {
 
   @override
   void initState() {
-    Timer.periodic(Duration(milliseconds: 200), (Timer t1) => _setSpeed(t1));
     _dB.listen((event) {_getWindFromDB(event);});
+  }
+
+  @override
+  dispose() {
+    _controller.dispose(); // you need this
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
-      body: Center(
-        child: SfRadialGauge(
-            axes:<RadialAxis>[RadialAxis(
-                showAxisLine: false,
-                showLabels: false,
-                showTicks: false,
-                pointers: <GaugePointer>[
-                  NeedlePointer(
-                      value: _finPos,
-                      lengthUnit: GaugeSizeUnit.factor,
-                      needleLength: 0.8,
-                      needleEndWidth:  3,
-                      tailStyle: const TailStyle(length: 0.2, width: 1,
-                        gradient:LinearGradient(
-                            colors: <Color>[
-                              Color(0xFFFFFAF0), Color(0xFFFFFAF0),
-                              Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
-                            stops: <double>[0, 0.5, 0.5, 1]),
-                      ),
-                      gradient: const LinearGradient(
-                          colors: <Color>[
-                            Color(0xFFFFFAF0), Color(0xFFFFFAF0),
-                            Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
-                          stops: <double>[0, 0.5, 0.5, 1]),
-                      needleColor: const Color(0xFFFFFAF0),
-                      knobStyle: const KnobStyle(
-                          knobRadius: 0.03,
-                          sizeUnit: GaugeSizeUnit.factor,
-                          color: Color(0xFFD1D9E1))),
-                  NeedlePointer(
-                      value: (_finPos + 90) % 360,
-                      lengthUnit: GaugeSizeUnit.factor,
-                      needleLength: 0.8,
-                      needleEndWidth: 3,
-                      tailStyle: const TailStyle(length: 0.2, width: 1,
-                        gradient:LinearGradient(
-                            colors: <Color>[
-                              Color(0xFFFFFAF0), Color(0xFFFFFAF0),
-                              Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
-                            stops: <double>[0, 0.5, 0.5, 1]),
-                      ),
-                      gradient: const LinearGradient(
-                          colors: <Color>[
-                            Color(0xFFFFFAF0), Color(0xFFFFFAF0),
-                            Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
-                          stops: <double>[0, 0.5, 0.5, 1]),
-                      needleColor: const Color(0xFFFFFAF0),
-                      knobStyle: const KnobStyle(
-                          knobRadius: 0.03,
-                          sizeUnit: GaugeSizeUnit.factor,
-                          color: Color(0xFFD1D9E1))),
-                  NeedlePointer(
-                      value: (_finPos + 180) % 360,
-                      lengthUnit: GaugeSizeUnit.factor,
-                      needleLength: 0.8,
-                      needleEndWidth:  3,
-                      tailStyle: const TailStyle(length: 0.2, width: 1,
-                        gradient:LinearGradient(
-                            colors: <Color>[
-                              Color(0xFFFFFAF0), Color(0xFFFFFAF0),
-                              Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
-                            stops: <double>[0, 0.5, 0.5, 1]),
-                      ),
-                      gradient: const LinearGradient(
-                          colors: <Color>[
-                            Color(0xFFFFFAF0), Color(0xFFFFFAF0),
-                            Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
-                          stops: <double>[0, 0.5, 0.5, 1]),
-                      needleColor: const Color(0xFFFFFAF0),
-                      knobStyle: const KnobStyle(
-                          knobRadius: 0.03,
-                          sizeUnit: GaugeSizeUnit.factor,
-                          color: Color(0xFFD1D9E1))),
-                  NeedlePointer(
-                      value: (_finPos + 270) % 360,
-                      lengthUnit: GaugeSizeUnit.factor,
-                      needleLength: 0.8,
-                      needleEndWidth:  3,
-                      tailStyle: const TailStyle(length: 0.2, width: 1,
-                        gradient:LinearGradient(
-                            colors: <Color>[
-                              Color(0xFFFFFAF0), Color(0xFFFFFAF0),
-                              Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
-                            stops: <double>[0, 0.5, 0.5, 1]),
-                      ),
-                      gradient: const LinearGradient(
-                          colors: <Color>[
-                            Color(0xFFFFFAF0), Color(0xFFFFFAF0),
-                            Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
-                          stops: <double>[0, 0.5, 0.5, 1]),
-                      needleColor: const Color(0xFFFFFAF0),
-                      knobStyle: const KnobStyle(
-                          knobRadius: 0.03,
-                          sizeUnit: GaugeSizeUnit.factor,
-                          color: Color(0xFFD1D9E1))),
-                  NeedlePointer(
-                      value: (_finPos + 360) % 360,
-                      lengthUnit: GaugeSizeUnit.factor,
-                      needleLength: 0.8,
-                      needleEndWidth:  3,
-                      tailStyle: const TailStyle(length: 0.2, width: 1,
-                        gradient:LinearGradient(
-                            colors: <Color>[
-                              Color(0xFFFFFAF0), Color(0xFFFFFAF0),
-                              Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
-                            stops: <double>[0, 0.5, 0.5, 1]),
-                      ),
-                      gradient: const LinearGradient(
-                          colors: <Color>[
-                            Color(0xFFFFFAF0), Color(0xFFFFFAF0),
-                            Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
-                          stops: <double>[0, 0.5, 0.5, 1]),
-                      needleColor: const Color(0xFFFFFAF0),
-                      knobStyle: const KnobStyle(
-                          knobRadius: 0.03,
-                          sizeUnit: GaugeSizeUnit.factor,
-                          color: Color(0xFFD1D9E1))),
-                ],
-                interval: 30, showFirstLabel: false,
-                startAngle: 270, endAngle: 270, minimum: 0, maximum: 360),
-            ],
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 228,
+            left: 316,
+            child: Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                  color: Colors.white,
+                ),
+                child:SizedBox(height:300.0,width:5),
+              ),
+            ),
+          ),
+          RotationTransition(
+                  turns: _animation,
+                  child: SfRadialGauge(
+                      axes:<RadialAxis>[RadialAxis(
+                          showAxisLine: false,
+                          showLabels: false,
+                          showTicks: false,
+                          pointers: const <GaugePointer>[
+                            NeedlePointer(
+                                value: 0,
+                                lengthUnit: GaugeSizeUnit.factor,
+                                needleLength: 0.5,
+                                needleEndWidth:  3,
+                                tailStyle: TailStyle(length: 0, width: 1,
+                                  gradient:LinearGradient(
+                                      colors: <Color>[
+                                        Color(0xFFFFFAF0), Color(0xFFFFFAF0),
+                                        Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
+                                      stops: <double>[0, 0.5, 0.5, 1]),
+                                ),
+                                gradient: LinearGradient(
+                                    colors: <Color>[
+                                      Color(0xFFFFFAF0), Color(0xFFFFFAF0),
+                                      Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
+                                    stops: <double>[0, 0.5, 0.5, 1]),
+                                needleColor: Color(0xFFFFFAF0),
+                                knobStyle: KnobStyle(
+                                    knobRadius: 0.03,
+                                    sizeUnit: GaugeSizeUnit.factor,
+                                    color: Color(0xFFD1D9E1))),
+                            NeedlePointer(
+                                value: 120,
+                                lengthUnit: GaugeSizeUnit.factor,
+                                needleLength: 0.5,
+                                needleEndWidth: 3,
+                                tailStyle: TailStyle(length: 0, width: 1,
+                                  gradient:LinearGradient(
+                                      colors: <Color>[
+                                        Color(0xFFFFFAF0), Color(0xFFFFFAF0),
+                                        Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
+                                      stops: <double>[0, 0.5, 0.5, 1]),
+                                ),
+                                gradient: LinearGradient(
+                                    colors: <Color>[
+                                      Color(0xFFFFFAF0), Color(0xFFFFFAF0),
+                                      Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
+                                    stops: <double>[0, 0.5, 0.5, 1]),
+                                needleColor: Color(0xFFFFFAF0),
+                                knobStyle: KnobStyle(
+                                    knobRadius: 0.03,
+                                    sizeUnit: GaugeSizeUnit.factor,
+                                    color: Color(0xFFD1D9E1))),
+                            NeedlePointer(
+                                value: 240,
+                                lengthUnit: GaugeSizeUnit.factor,
+                                needleLength: 0.5,
+                                needleEndWidth:  3,
+                                tailStyle: TailStyle(length: 0, width: 1,
+                                  gradient:LinearGradient(
+                                      colors: <Color>[
+                                        Color(0xFFFFFAF0), Color(0xFFFFFAF0),
+                                        Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
+                                      stops: <double>[0, 0.5, 0.5, 1]),
+                                ),
+                                gradient: LinearGradient(
+                                    colors: <Color>[
+                                      Color(0xFFFFFAF0), Color(0xFFFFFAF0),
+                                      Color(0xFFE1D9D1), Color(0xFFE1D9D1)],
+                                    stops: <double>[0, 0.5, 0.5, 1]),
+                                needleColor: Color(0xFFFFFAF0),
+                                knobStyle: KnobStyle(
+                                    knobRadius: 0.03,
+                                    sizeUnit: GaugeSizeUnit.factor,
+                                    color: Color(0xFFD1D9E1))),
 
-        ),
+                          ],
+                          interval: 30, showFirstLabel: false,
+                          startAngle: 270, endAngle: 270, minimum: 0, maximum: 360),
+                      ],
+
+                  ),
+                ),
+
+        ],
       ),
     );
   }
