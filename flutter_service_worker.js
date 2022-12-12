1951c023f9b2c22528f11f978e1756ee937e57b1
Isaac Allen <69442967+IsaacMAllen@users.noreply.github.com>
2022-12-12 13:37:25 -0500

    rebuild

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -41,9 +41,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e37d0f395579aee020529b5d817aa6f8",
-"/": "e37d0f395579aee020529b5d817aa6f8",
-"main.dart.js": "1eaa7498e7763ea5becf40d6ef0b8990",
+"index.html": "5e0b1dd707367b902767d3fe16b5335f",
+"/": "5e0b1dd707367b902767d3fe16b5335f",
+"main.dart.js": "89c3dace2c3a0d720f53619251220646",
 "manifest.json": "e4f75c51a5713a284ba04e017603455e",
 "version.json": "61f560357dafbd4b2783abb86fb63836"
 };
